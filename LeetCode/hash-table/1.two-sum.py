298c91fc79afb656a780876c1ad01e6a6dc8f24c
Neha Jha <nehajhatcet@gmail.com>
2020-10-06 18:40:28 +0530

    two Sum

--- LeetCode/hash-table/1.two-sum.py
+++ LeetCode/hash-table/1.two-sum.py
@@ -12,5 +12,4 @@ class Solution:
         for i in range(len(nums)):
             if target - nums[i] in d and i != d[target-nums[i]]:
                 return i, d[target-nums[i]]
-
 # @lc code= end
